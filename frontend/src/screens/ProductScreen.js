55538de73a613932d5cc1888ee0076e3563d1c27
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-01-27 22:20:37 -0300

    Adding Redux to Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,81 +1,98 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import data from '../data';
 import Rating from '../components/Rating';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
-    <Fragment>
-      <Header />
-      <div className="pruduct-main-page">
-        <Link to="/">Back to result</Link>
-        <div className="row top">
-          <div className="col-2">
-            <img className="large" src={product.image} alt={product.name}></img>
-          </div>
-          <div className="col-1">
-            <ul>
-              <li>
-                <h1>{product.name}</h1>
-              </li>
-              <li>
-                <div className="row">
-                  <Rating
-                    rating={product.rating}
-                    numReviews={product.numReviews}
-                  ></Rating>
-                  ({product.numReviews} Reviews)
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+            <Fragment>
+              <Header />
+              <div className="pruduct-main-page">
+                <Link to="/">Back to result</Link>
+                <div className="row top">
+                  <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name}></img>
+                  </div>
+                  <div className="col-1">
+                    <ul>
+                      <li>
+                        <h1>{product.name}</h1>
+                      </li>
+                      <li>
+                        <div className="row">
+                          <Rating
+                            rating={product.rating}
+                            numReviews={product.numReviews}
+                          ></Rating>
+                          ({product.numReviews} Reviews)
+                        </div>
+                      </li>
+                      <li>Price: ${product.price}</li>
+                      <li>
+                        Description:
+                        <p className="description-text">{product.description}</p>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col-1">
+                    <div className="card card-body">
+                      <ul>
+                        <li>
+                          <div className="row">
+                            <div>Price:</div>
+                            <div className="price">${product.price}</div>
+                          </div>
+                        </li>
+                        <li>
+                          <div className="row">
+                            <div>Status:</div>
+                            <div>
+                              {product.countInStock > 0 ? (
+                                <span className="success">In Stock</span>
+                              ) : (
+                                  <span className="danger">Unavailable</span>
+                                )}
+                            </div>
+                          </div>
+                        </li>
+                        <li>
+                          <div className="row">
+                            <button className="primary block">Add to Cart</button>
+                            <div className="box-price">
+                              | <span><i class="fas fa-shopping-basket"></i></span>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-              </li>
-              <li>Price: ${product.price}</li>
-              <li>
-                Description:
-                <p className="description-text">{product.description}</p>
-              </li>
-            </ul>
-          </div>
-          <div className="col-1">
-            <div className="card card-body">
-              <ul>
-                <li>
-                  <div className="row">
-                    <div>Price:</div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="row">
-                    <div>Status:</div>
-                    <div>
-                      {product.countInStock > 0 ? (
-                        <span className="success">In Stock</span>
-                      ) : (
-                          <span className="danger">Unavailable</span>
-                        )}
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="row">
-                    <button className="primary block">Add to Cart</button>
-                    <div className="box-price">
-                      | <span><i class="fas fa-shopping-basket"></i></span>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </Fragment>
+              </div>
+              <Footer />
+            </Fragment>
+          )
+      }
+    </div>
   )
 }
 
