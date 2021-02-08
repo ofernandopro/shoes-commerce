8980973acfcaa9ee8a8bb84a0c3511fd7c2c3b2e
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-02-08 16:50:51 -0300

    Create Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/shipping" component={ShippingAddressScreen} />
         <Route path="/payment" component={PaymentMethodScreen} />
         <Route path="/placeorder" component={PlaceOrderScreen} />
+        <Route path="/order/:id" component={OrderScreen} />
         <Route exact path="/" component={HomeScreen} />
       </Switch>
     </Router>
---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,136 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { detailsOrder } from '../actions/orderActions';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+function OrderScreen(props) {
+  const orderId = props.match.params.id;
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(detailsOrder(orderId));
+  }, [dispatch, orderId]);
+
+  return loading? (<LoadingBox></LoadingBox>):
+  error? (<MessageBox variant="danger">{error}</MessageBox>)
+  :
+  (
+    <div>
+      <Header />
+      <h1>Order {order._id}</h1>
+      <div className="row top place-order-screen">
+        <div className="col-2">
+          <div className="card-place-order">
+            <div className="card">
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name:</strong> {order.shippingAddress.fullName} <br />
+                <strong>Address: </strong> {order.shippingAddress.address},{' '}
+                {order.shippingAddress.city}, {order.shippingAddress.postalCode},{' '} 
+                {order.shippingAddress.country}.
+              </p>
+              {order.isDelivered ? (
+                <MessageBox variant="success">
+                  Delivered at {order.deliveredAt}
+                </MessageBox>
+              ) : (
+                <MessageBox variant="danger">
+                  Not Delivered
+                </MessageBox>
+              )}
+            </div>
+
+            <hr />
+            
+            <div className="card">
+              <h2>Payment</h2>
+              <p>
+                <strong>Method:</strong> {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <MessageBox variant="success">
+                  Paid at {order.paidAt}
+                </MessageBox>
+              ) : (
+                <MessageBox variant="danger">
+                  Not Paid
+                </MessageBox>
+              )}
+            </div>
+
+            <hr />
+
+            <div>
+              <div className="card">
+                <h2>Order Items</h2>
+                <ul>
+                {
+                  order.orderItems.map((item) => (
+                    <li key={item.product}>
+                      <div className="row">
+                        <div>
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="small"
+                          />
+                        </div>
+                        <div className="cart-product-name min-30">
+                          <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        </div>
+                        
+                        <div className="cart-section-price">
+                          {item.qnt} x ${item.price.toFixed(2)} = ${(item.qnt * item.price).toFixed(2)}
+                        </div>
+                        
+                      </div>
+                    </li>
+                  ))
+                }
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-1">
+          <div className="card card-body card-body-place-order">
+              <div>
+                <h2>Order Summary</h2>
+              </div>
+
+              <div className="row">
+                <div>Items</div>
+                <div>${order.itemsPrice.toFixed(2)}</div>
+              </div>
+
+              <div className="row">
+                <div>Shipping</div>
+                <div>${order.shippingPrice.toFixed(2)}</div>
+              </div>
+
+              <div className="row">
+                <div>Tax</div>
+                <div>${order.taxPrice.toFixed(2)}</div>
+              </div>
+
+              <div className="row">
+                <div><strong>Order Total</strong></div>
+                <div><strong>${order.totalPrice.toFixed(2)}</strong></div>
+              </div>
+
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default OrderScreen;
